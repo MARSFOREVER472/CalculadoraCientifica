d917f67e0b1bcf6ee78aacaa0606de37c03ec718
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-17 21:31:22 -0400

    2 archivos adjuntos por aquí

---
+++ Calculadora.js
@@ -0,0 +1,33 @@
+// VAMOS A  CREAR UNA FUNCIÓN PARA ENLAZAR OPERACIONES MATEMÁTICAS A LA PÁGINA WEB:
+
+function LetsDoMath(type)
+{
+    // LLAMADO A LAS 2 VARIABLES NUMÉRICAS...
+
+    var numberA = parseInt(document.getElementById("numA").value); // NÚMERO 1.
+    var numberB = parseInt(document.getElementById("numB").value); // NÚMERO 2.
+
+    var total = document.getElementById("total");
+
+    // PARA TRABAJAR CON ALGUNAS DE LAS OPERACIONES ARITMÉTICAS, SELECCIONAREMOS UNA DE ÉSTAS
+    // UTILIZANDO LA ITERACIÓN "switch"...
+
+    switch(type)
+    {
+        case "add": // SI ES UNA SUMA...
+            total.innerHTML = "Numero total: " + (numberA + numberB);
+            break;
+        case "subtract": // SI ES UNA RESTA...
+            total.innerHTML = "Numero total: " + (numberA - numberB);
+            break;
+        case "multiply": // SI ES UNA MULTIPLICACIÓN...
+            total.innerHTML = "Numero total: " + (numberA * numberB);
+            break;
+        case "divide": // SI ES UNA DIVISIÓN Y SE TRABAJA CON NÚMEROS DECIMALES Y/O ENTEROS...
+
+            var decimal = parseFloat(numberA / numberB).toFixed(2); // CUANDO ES UN NÚMERO DECIMAL...
+
+            total.innerHTML = "Numero total: " + decimal;
+            break;
+    }
+}
