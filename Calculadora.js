5befdf8c13c9edef19234d0438d4c5972f85ed3f
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-06-24 18:44:47 -0400

    Se agregó un valor más a efectuar...

--- Calculadora.js
+++ Calculadora.js
@@ -6,6 +6,7 @@ function LetsDoMath(type)
 
     var numberA = parseInt(document.getElementById("numA").value); // NÚMERO 1.
     var numberB = parseInt(document.getElementById("numB").value); // NÚMERO 2.
+    var numberC = parseInt(document.getElementById("numC").value); // NÚMERO 3.
 
     var total = document.getElementById("total");
 
@@ -15,17 +16,17 @@ function LetsDoMath(type)
     switch(type)
     {
         case "add": // SI ES UNA SUMA...
-            total.innerHTML = "Numero total: " + (numberA + numberB);
+            total.innerHTML = "Numero total: " + (numberA + numberB + numberC);
             break;
         case "subtract": // SI ES UNA RESTA...
-            total.innerHTML = "Numero total: " + (numberA - numberB);
+            total.innerHTML = "Numero total: " + (numberA - numberB - numberC);
             break;
         case "multiply": // SI ES UNA MULTIPLICACIÓN...
-            total.innerHTML = "Numero total: " + (numberA * numberB);
+            total.innerHTML = "Numero total: " + (numberA * numberB * numberC);
             break;
         case "divide": // SI ES UNA DIVISIÓN Y SE TRABAJA CON NÚMEROS DECIMALES Y/O ENTEROS...
 
-            var decimal = parseFloat(numberA / numberB).toFixed(2); // CUANDO ES UN NÚMERO DECIMAL...
+            var decimal = parseFloat(numberA / numberB / numberC).toFixed(2); // CUANDO ES UN NÚMERO DECIMAL...
 
             total.innerHTML = "Numero total: " + decimal;
             break;
